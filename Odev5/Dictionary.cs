db753958966e6e60c9d918be6f67871254823464
sbayram93 <mikrozom@gmail.com>
2021-01-30 16:00:42 +0300

    Add project files.

---
+++ Odev5/Dictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] schoolNumber;
+        V[] schoolClasses;
+
+        public MyDictionary()
+        {
+            schoolNumber = new K[0];
+            schoolClasses = new V[0];
+        }
+
+        public void Add(K number, V classes)
+        {
+            K[] tempSchoolNumber = schoolNumber;
+            schoolNumber = new K[schoolNumber.Length + 1];
+            schoolClasses = new V[schoolClasses.Length + 1];
+
+            for (int i = 0; i < tempSchoolNumber.Length; i++)
+            {
+                schoolNumber[i] = tempSchoolNumber[i];
+            }
+            for (int i = 0; i < schoolClasses.Length; i++)
+            {
+                V[] tempSchoolClasses = classes;
+                schoolClasses[i] = tempSchoolClasses[i];
+            }
+
+            schoolNumber[schoolNumber.Length - 1] = number;
+            schoolClasses[schoolClasses.Length - 1] = classes;
+
+        }
+
+        public K[] key
+        {
+            get { return schoolNumber; }
+        }
+        public V[] value
+        {
+            get { return schoolClasses; }
+        }
+
+        public int Length
+        {
+            get { return schoolNumber.Length; }
+        }
+
+    }
+}
---
+++ Odev5/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> schoolNumber = new MyDictionary<int, string>();
+            schoolNumber.Add(112, "7-A");
+            schoolNumber.Add(134, "7-C");
+            schoolNumber.Add(211, "6-B");
+
+            int j = 0;
+
+            for (int i = 0; i < schoolNumber.Length; i++)
+            {
+                Console.WriteLine(j + ". Kayıtlı öğrenci numarası : " + schoolNumber.value[i] + " Sınıfı : " + schoolNumber.key[i]);
+                j = j + 1;
+            }
+
+        }
+    }
+}
